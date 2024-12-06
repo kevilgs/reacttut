8be6e02bd63fdecb61ece679c9319decac441fc9
Kevil Shaji <kevilshaji@student.sfit.ac.in>
2024-12-06 20:19:49 +0530

    just the begining

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+    <nav
+      className="navbar navbar-expand-sm navbar-dark bg-dark"
+    >
+      <div className="container">
+        <a className="navbar-brand" href="/">Textutils</a>
+        <button
+          className="navbar-toggler d-lg-none"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapsibleNavId"
+          aria-controls="collapsibleNavId"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="collapsibleNavId">
+          <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" href="/" aria-current="page"
+                >Home
+                <span className="visually-hidden">(current)</span></a
+              >
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">About</a>
+            </li>
+          </ul>
+          <form className="d-flex my-2 my-lg-0">
+            <input
+              className="form-control me-sm-2"
+              type="text"
+              placeholder="Search"
+            />
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
+    
+    </>
   );
 }
 
